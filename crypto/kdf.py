63f92ab833a69bf7ecd60022f6566218c2c63085
Sameer Mallik <main.smallik@gmail.com>
2026-05-22 12:46:04 -0700

    move salt generation to helper function

--- crypto/kdf.py
+++ crypto/kdf.py
@@ -7,12 +7,14 @@ ITERATIONS = 600000
 KEY_LENGTH = 32
 SALT_LENGTH = 16
 
-def derive_key(password: str) -> tuple[bytes, bytes]:
-    salt = os.urandom(SALT_LENGTH)
+def generate_salt() -> bytes:
+    return os.urandom(SALT_LENGTH)
+
+def derive_key(password: str, salt: bytes) -> bytes:
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=KEY_LENGTH,
         salt=salt,
         iterations=ITERATIONS,
     )
-    return base64.urlsafe_b64encode(kdf.derive(password.encode())), salt
+    return base64.urlsafe_b64encode(kdf.derive(password.encode()))
