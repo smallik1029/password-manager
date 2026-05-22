62554b45df70f811d3633c3b6401ef0d005a63b9
Sameer Mallik <main.smallik@gmail.com>
2026-05-22 13:04:54 -0700

    add decryption with error handling

--- crypto/cipher.py
+++ crypto/cipher.py
@@ -1,5 +1,12 @@
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet, InvalidToken
 
 def encrypt(data: bytes, key: bytes) -> bytes:
     f = Fernet(key)
     return f.encrypt(data)
+
+def decrypt(token: bytes, key: bytes) -> bytes:
+    f = Fernet(key)
+    try:
+        return f.decrypt(token)
+    except InvalidToken:
+        raise ValueError("Decryption Failed.")
